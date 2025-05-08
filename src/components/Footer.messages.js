b6003d6c4894b37c644cb166d5b971576dc3a7a5
tenkalden <tenkalden007@gmail.com>
2025-05-08 14:20:20 +0530

    message change

--- src/components/Footer.messages.js
+++ src/components/Footer.messages.js
@@ -153,7 +153,7 @@ const messages = defineMessages({
   },
   'footer.logo.hoverText': {
     id: 'footer.logo.hoverText',
-    defaultMessage: 'Go to home',
+    defaultMessage: 'Go to main page',
     description: 'Hover text for the footer logo that indicates clicking will return to home page.',
   },
 });
