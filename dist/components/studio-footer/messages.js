e8c4c94a377fb3826a09b7a54dd4cc0678f1d19f
tenkalden <tenkalden007@gmail.com>
2025-05-28 12:19:21 +0530

    added dist

--- dist/components/studio-footer/messages.js
+++ dist/components/studio-footer/messages.js
@@ -1,5 +1,5 @@
 import { defineMessages } from '@edx/frontend-platform/i18n';
-const messages = defineMessages({
+var messages = defineMessages({
   openHelpButtonLabel: {
     id: 'authoring.footer.help.openHelp.button.label',
     defaultMessage: 'Looking for help with Studio?',
