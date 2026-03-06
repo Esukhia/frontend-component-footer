d3d9a8b3b061393b11e485a3f44d53957a4bb6b7
Tenzin Choeying <tenzinchoeying@Tenzins-MacBook-Pro.local>
2026-03-06 16:56:33 +0530

    Added dist

--- dist/plugin-slots/StudioFooterLogoSlot/index.js
+++ dist/plugin-slots/StudioFooterLogoSlot/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { PluginSlot } from '@openedx/frontend-plugin-framework';
 import { Hyperlink, Image } from '@openedx/paragon';
+import sherabLogo from '../StudioFooterLogoSlot/images/logo.png';
 const StudioFooterLogoSlot = () => /*#__PURE__*/React.createElement(PluginSlot, {
   id: "org.openedx.frontend.layout.studio_footer_logo.v1",
   idAliases: ['studio_footer_logo_slot']
@@ -8,9 +9,9 @@ const StudioFooterLogoSlot = () => /*#__PURE__*/React.createElement(PluginSlot, 
   destination: "https://openedx.org",
   className: "float-right"
 }, /*#__PURE__*/React.createElement(Image, {
-  width: "120px",
-  alt: "Powered by Open edX",
-  src: "https://logos.openedx.org/open-edx-logo-tag.png"
+  width: "50px",
+  alt: "Sherab",
+  src: sherabLogo
 })));
 export default StudioFooterLogoSlot;
 //# sourceMappingURL=index.js.map
