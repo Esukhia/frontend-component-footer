67a8f999260284046c27dff1ac97b4fb4962c4f4
Tenzin Choeying <tenzinchoeying@Tenzins-MacBook-Pro-4.local>
2026-08-05 15:26:46 +0530

    Replaced the logo and its alt to webuddhist academy in frontend-component-footer

--- src/plugin-slots/StudioFooterLogoSlot/index.jsx
+++ src/plugin-slots/StudioFooterLogoSlot/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { PluginSlot } from '@openedx/frontend-plugin-framework';
 import { Hyperlink, Image } from '@openedx/paragon';
-import sherabLogo from '../StudioFooterLogoSlot/images/logo.png'
+import wbaLogo from '../StudioFooterLogoSlot/images/logo.png'
 const StudioFooterLogoSlot = () => (
   <PluginSlot id="org.openedx.frontend.layout.studio_footer_logo.v1" idAliases={['studio_footer_logo_slot']}>
     <Hyperlink destination="https://openedx.org" className="float-right">
       <Image
         width="50px"
-        alt="Sherab"
-        src={sherabLogo}
+        alt="WeBuddhist Academy"
+        src={wbaLogo}
       />
     </Hyperlink>
   </PluginSlot>
